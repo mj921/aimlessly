5914c5f6150d887ec0be1eb47e1678ebbc031ddc
majie <373002712@qq.com>
2022-06-01 15:46:14 +0800

    合成龙 路线

---
+++ mergeDragons/link.js
@@ -0,0 +1,551 @@
+const links = [{
+  "source": "生命之花幼芽",
+  "target": "生命之花",
+  "type": "合成"
+},
+{
+  "source": "生命之花",
+  "target": "蓝色生命之花",
+  "type": "合成"
+},
+{
+  "source": "蓝色生命之花",
+  "target": "发光的生命之花",
+  "type": "合成"
+},
+{
+  "source": "发光的生命之花",
+  "target": "双生生命之花",
+  "type": "合成"
+},
+{
+  "source": "双生生命之花",
+  "target": "闪耀的生命之花",
+  "type": "合成"
+},
+{
+  "source": "闪耀的生命之花",
+  "target": "巨型生命之花",
+  "type": "合成"
+},
+{
+  "source": "巨型生命之花",
+  "target": "生命之树幼芽",
+  "type": "合成"
+},
+{
+  "source": "生命之树幼芽",
+  "target": "生命之树幼苗",
+  "type": "合成"
+},
+{
+  "source": "生命之树幼苗",
+  "target": "生根的生命之树",
+  "type": "合成"
+},
+{
+  "source": "生根的生命之树",
+  "target": "奇特的生命之树",
+  "type": "合成"
+},
+{
+  "source": "奇特的生命之树",
+  "target": "纤细的生命之树",
+  "type": "合成"
+},
+{
+  "source": "纤细的生命之树",
+  "target": "惊奇的生命之树",
+  "type": "合成"
+},
+{
+  "source": "龙血树树叶",
+  "target": "龙血树苗",
+  "type": "合成"
+},
+{
+  "source": "龙血树种子",
+  "target": "龙血树苗",
+  "type": "合成/生长"
+},
+{
+  "source": "龙血树苗",
+  "target": "发芽的龙血树",
+  "type": "合成"
+},
+{
+  "source": "发芽的龙血树",
+  "target": "成长期的龙血树",
+  "type": "合成"
+},
+{
+  "source": "成长期的龙血树",
+  "target": "朱红色龙血树",
+  "type": "合成"
+},
+{
+  "source": "朱红色龙血树",
+  "target": "漂亮的龙血树",
+  "type": "合成"
+},
+{
+  "source": "漂亮的龙血树",
+  "target": "成年龙血树",
+  "type": "合成"
+},
+{
+  "source": "成年龙血树",
+  "target": "老年龙血树",
+  "type": "合成"
+},
+{
+  "source": "老年龙血树",
+  "target": "古老龙血树",
+  "type": "合成"
+},
+{
+  "source": "古老龙血树",
+  "target": "神秘龙血树",
+  "type": "合成"
+},
+{
+  "source": "棱之花种子",
+  "target": "发芽的棱之花",
+  "type": "合成/生长"
+},
+{
+  "source": "发芽的棱之花",
+  "target": "棱之花花蕾",
+  "type": "合成"
+},
+{
+  "source": "棱之花花蕾",
+  "target": "开放的棱之花",
+  "type": "合成"
+},
+{
+  "source": "开放的棱之花",
+  "target": "棱之花",
+  "type": "合成"
+},
+{
+  "source": "棱之花",
+  "target": "绽放的棱之花",
+  "type": "合成"
+},
+{
+  "source": "绽放的棱之花",
+  "target": "华丽的棱之花",
+  "type": "合成"
+},
+{
+  "source": "华丽的棱之花",
+  "target": "灿烂的棱之花",
+  "type": "合成"
+},
+{
+  "source": "灿烂的棱之花",
+  "target": "鬼魅的棱之花",
+  "type": "合成"
+},
+{
+  "source": "鬼魅的棱之花",
+  "target": "发光的棱之花",
+  "type": "合成"
+},
+{
+  "source": "发光的棱之花",
+  "target": "三位一体穹顶",
+  "type": "合成"
+},
+{
+  "source": "新石",
+  "target": "带苔藓的石头",
+  "type": "合成/采集"
+},
+{
+  "source": "带苔藓的石头",
+  "target": "活石",
+  "type": "合成"
+},
+{
+  "source": "活石",
+  "target": "大型苔藓石",
+  "type": "合成"
+},
+{
+  "source": "大型苔藓石",
+  "target": "三叶草石",
+  "type": "合成"
+},
+{
+  "source": "三叶草石",
+  "target": "玄武岩",
+  "type": "合成"
+},
+{
+  "source": "玄武岩",
+  "target": "国王的座椅",
+  "type": "合成"
+},
+{
+  "source": "国王的座椅",
+  "target": "恐龙岩",
+  "type": "合成"
+},
+{
+  "source": "恐龙岩",
+  "target": "蓝苔石像",
+  "type": "合成"
+},
+{
+  "source": "蓝苔石像",
+  "target": "龙苔草原",
+  "type": "合成"
+},
+{
+  "source": "一块石材",
+  "target": "石材",
+  "type": "合成"
+},
+{
+  "source": "石材",
+  "target": "石材堆",
+  "type": "合成"
+},
+{
+  "source": "石材堆",
+  "target": "一个暗石材",
+  "type": "合成"
+},
+{
+  "source": "一个暗石材",
+  "target": "暗石材",
+  "type": "合成"
+},
+{
+  "source": "暗石材",
+  "target": "暗石材堆",
+  "type": "合成"
+},
+{
+  "source": "暗石材堆",
+  "target": "食人魔石头碎片",
+  "type": "合成"
+},
+{
+  "source": "食人魔石头碎片",
+  "target": "食人魔石头",
+  "type": "合成"
+},
+{
+  "source": "食人魔石头",
+  "target": "食人魔石头巨块",
+  "type": "合成"
+},
+{
+  "source": "带苔藓的石头",
+  "target": "一块石材",
+  "type": "采集"
+},
+{
+  "source": "活石",
+  "target": "一块石材",
+  "type": "采集"
+},
+{
+  "source": "大型苔藓石",
+  "target": "一块石材",
+  "type": "采集"
+},
+{
+  "source": "三叶草石",
+  "target": "石材",
+  "type": "采集"
+},
+{
+  "source": "玄武岩",
+  "target": "石材",
+  "type": "采集"
+},
+{
+  "source": "国王的座椅",
+  "target": "石材堆",
+  "type": "采集"
+},
+{
+  "source": "恐龙岩",
+  "target": "石材堆",
+  "type": "采集"
+},
+{
+  "source": "蓝苔石像",
+  "target": "一个暗石材",
+  "type": "采集"
+},
+{
+  "source": "龙苔草原",
+  "target": "一个暗石材",
+  "type": "采集"
+},
+{
+  "source": "生命精华",
+  "target": "微型生命之球",
+  "type": "合成"
+},
+{
+  "source": "微型生命之球",
+  "target": "小生命之球",
+  "type": "合成"
+},
+{
+  "source": "小生命之球",
+  "target": "生命之球",
+  "type": "合成"
+},
+{
+  "source": "生命之球",
+  "target": "强力生命之球",
+  "type": "合成"
+},
+{
+  "source": "强力生命之球",
+  "target": "奇妙生命之球",
+  "type": "合成"
+},
+{
+  "source": "奇妙生命之球",
+  "target": "大型生命之球",
+  "type": "合成"
+},
+{
+  "source": "大型生命之球",
+  "target": "巨型生命之球",
+  "type": "合成"
+},
+{
+  "source": "巨型生命之球",
+  "target": "灵魂生命之球",
+  "type": "合成"
+},
+{
+  "source": "灵魂生命之球",
+  "target": "天堂生命之球",
+  "type": "合成"
+},
+{
+  "source": "生命之花",
+  "target": "生命精华",
+  "type": "采集"
+},
+{
+  "source": "蓝色生命之花",
+  "target": "生命精华",
+  "type": "采集"
+},
+{
+  "source": "发光的生命之花",
+  "target": "微型生命之球",
+  "type": "采集"
+},
+{
+  "source": "双生生命之花",
+  "target": "微型生命之球",
+  "type": "采集"
+},
+{
+  "source": "闪耀的生命之花",
+  "target": "小生命之球",
+  "type": "采集"
+},
+{
+  "source": "巨型生命之花",
+  "target": "小生命之球",
+  "type": "采集"
+},
+{
+  "source": "生命之树幼芽",
+  "target": "生命之球",
+  "type": "采集"
+},
+{
+  "source": "生命之树幼苗",
+  "target": "生命之球",
+  "type": "采集"
+},
+{
+  "source": "生根的生命之树",
+  "target": "强力生命之球",
+  "type": "采集"
+},
+{
+  "source": "奇特的生命之树",
+  "target": "强力生命之球",
+  "type": "采集"
+},
+{
+  "source": "纤细的生命之树",
+  "target": "奇妙生命之球",
+  "type": "采集"
+},
+{
+  "source": "惊奇的生命之树",
+  "target": "奇妙生命之球",
+  "type": "采集"
+},
+{
+  "source": "神秘种子",
+  "target": "草丛",
+  "type": "合成"
+},
+{
+  "source": "草丛",
+  "target": "草坪",
+  "type": "合成"
+},
+{
+  "source": "草坪",
+  "target": "大草地",
+  "type": "合成"
+},
+{
+  "source": "大草地",
+  "target": "沼泽草地",
+  "type": "合成"
+},
+{
+  "source": "沼泽草地",
+  "target": "高草",
+  "type": "合成"
+},
+{
+  "source": "高草",
+  "target": "金色麦草",
+  "type": "合成"
+},
+{
+  "source": "金色麦草",
+  "target": "巨龙的触须",
+  "type": "合成"
+},
+{
+  "source": "巨龙的触须",
+  "target": "发光的草地",
+  "type": "合成"
+},
+{
+  "source": "草坪",
+  "target": "新石",
+  "type": "采集"
+},
+{
+  "source": "大草地",
+  "target": "新石",
+  "type": "采集"
+},
+{
+  "source": "沼泽草地",
+  "target": "新石",
+  "type": "采集"
+},
+{
+  "source": "高草",
+  "target": "带苔藓的石头",
+  "type": "采集"
+},
+{
+  "source": "金色麦草",
+  "target": "带苔藓的石头",
+  "type": "采集"
+},
+{
+  "source": "巨龙的触须",
+  "target": "带苔藓的石头",
+  "type": "采集"
+},
+{
+  "source": "发光的草地",
+  "target": "带苔藓的石头",
+  "type": "采集"
+},
+{
+  "source": "普通宝箱",
+  "target": "漂亮的宝箱",
+  "type": "合成"
+},
+{
+  "source": "漂亮的宝箱",
+  "target": "锁住的宝箱",
+  "type": "合成"
+},
+{
+  "source": "锁住的宝箱",
+  "target": "沉没之箱",
+  "type": "合成"
+},
+{
+  "source": "沉没之箱",
+  "target": "贵族箱",
+  "type": "合成"
+},
+{
+  "source": "贵族箱",
+  "target": "发光的箱子",
+  "type": "合成"
+},
+{
+  "source": "发光的箱子",
+  "target": "巨型宝箱",
+  "type": "合成"
+},
+{
+  "source": "沼泽草地",
+  "target": "漂亮的宝箱",
+  "type": "采集"
+},
+{
+  "source": "沼泽草地",
+  "target": "普通的宝箱",
+  "type": "采集"
+},
+{
+  "source": "玄武岩",
+  "target": "锁住的宝箱",
+  "type": "采集"
+},
+{
+  "source": "发芽的棱之花",
+  "target": "生命精华",
+  "type": "采集"
+},
+{
+  "source": "棱之花花蕾",
+  "target": "龙血树树叶",
+  "type": "采集"
+},
+{
+  "source": "灌木幼芽",
+  "target": "萌芽的灌木",
+  "type": "合成"
+},
+{
+  "source": "萌芽的灌木",
+  "target": "漂亮的灌木",
+  "type": "合成"
+},
+{
+  "source": "漂亮的灌木",
+  "target": "有花灌木",
+  "type": "合成"
+},
+{
+  "source": "有花灌木",
+  "target": "华丽的灌木",
+  "type": "合成"
+},
+{
+  "source": "华丽的灌木",
+  "target": "天宫遗迹",
+  "type": "合成"
+},
+]
